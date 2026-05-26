5e22de93383df8e2f6960bf681550ba6479b01af
lucieleblanc <lucieleblanc@users.noreply.github.com>
2026-05-26 16:52:09 -0400

    Carry ancestor gitignore status when checking gitignores top-down (#11698)
    (cherry picked from commit bc4ccd1808532f761a39c3505e57d748f0e53552)

--- crates/repo_metadata/src/local_model.rs
+++ crates/repo_metadata/src/local_model.rs
@@ -587,7 +587,7 @@ impl LocalRepoMetadataModel {
                 let mut files = Vec::new();
                 let mut gitignores = gitignores.to_owned();
                 let mut file_limit = MAX_FILES_PER_REPO;
-                match Entry::build_tree(
+                match Entry::build_tree_with_ignored_ancestor(
                     path_to_add,
                     &mut files,
                     &mut gitignores,
@@ -595,6 +595,7 @@ impl LocalRepoMetadataModel {
                     MAX_TREE_DEPTH,
                     0,
                     &IgnoredPathStrategy::IncludeLazy,
+                    is_ignored,
                 ) {
                     Ok(subtree) => {
                         mutations.push(FileTreeMutation::AddDirectorySubtree {
