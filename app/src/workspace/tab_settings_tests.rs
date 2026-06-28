793dcd6cb92bb1b9c4daeca77d485d505da717cb
Timofey Alexeyvich Makhlay Kapitsa <tmakhlay2@gmail.com>
2026-06-28 17:22:26 +0200

    fix(tabs): suppressed directory shadows broader tab-color prefix (#100)

--- app/src/workspace/tab_settings_tests.rs
+++ app/src/workspace/tab_settings_tests.rs
@@ -4,6 +4,80 @@ use settings::Setting;
 use warpui::{App, SingletonEntity};
 
 #[test]
+fn color_for_directory_uses_longest_prefix_color_match() {
+    let colors = DirectoryTabColors(
+        [
+            (
+                "/a".to_string(),
+                DirectoryTabColor::Color(AnsiColorIdentifier::Red),
+            ),
+            (
+                "/a/b".to_string(),
+                DirectoryTabColor::Color(AnsiColorIdentifier::Green),
+            ),
+        ]
+        .into_iter()
+        .collect(),
+    );
+
+    assert_eq!(
+        colors.color_for_directory(Path::new("/a/b/c")),
+        Some(DirectoryTabColor::Color(AnsiColorIdentifier::Green))
+    );
+}
+
+#[test]
+fn color_for_directory_suppressed_shadows_broader_prefix() {
+    let colors = DirectoryTabColors(
+        [
+            (
+                "/a".to_string(),
+                DirectoryTabColor::Color(AnsiColorIdentifier::Red),
+            ),
+            ("/a/b".to_string(), DirectoryTabColor::Suppressed),
+        ]
+        .into_iter()
+        .collect(),
+    );
+
+    assert_eq!(colors.color_for_directory(Path::new("/a/b/c")), None);
+}
+
+#[test]
+fn color_for_directory_broader_prefix_still_applies_outside_suppressed_subtree() {
+    let colors = DirectoryTabColors(
+        [
+            (
+                "/a".to_string(),
+                DirectoryTabColor::Color(AnsiColorIdentifier::Red),
+            ),
+            ("/a/b".to_string(), DirectoryTabColor::Suppressed),
+        ]
+        .into_iter()
+        .collect(),
+    );
+
+    assert_eq!(
+        colors.color_for_directory(Path::new("/a/x")),
+        Some(DirectoryTabColor::Color(AnsiColorIdentifier::Red))
+    );
+}
+
+#[test]
+fn color_for_directory_returns_none_without_configured_prefix() {
+    let colors = DirectoryTabColors(
+        [(
+            "/b".to_string(),
+            DirectoryTabColor::Color(AnsiColorIdentifier::Blue),
+        )]
+        .into_iter()
+        .collect(),
+    );
+
+    assert_eq!(colors.color_for_directory(Path::new("/a/b/c")), None);
+}
+
+#[test]
 fn use_latest_user_prompt_as_conversation_title_in_tab_names_defaults_to_false() {
     App::test((), |mut app| async move {
         initialize_settings_for_tests(&mut app);
