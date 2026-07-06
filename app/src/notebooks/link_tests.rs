Drew1811266/Warp-CN

Respect Markdown Viewer preference for Markdown file links (#9483)
```bash
cargo test -p warp --lib notebooks::link::tests::test_open_markdown_file --features local_fs --no-run
```

app/src/notebooks/link_tests.rs
@@ -9,14 +9,16 @@ use parking_lot::Mutex;
 use tempfile::tempdir;
 use url::Url;
 use warp_util::path::LineAndColumnArg;
-use warpui::{App, ModelHandle, WindowId};
+use warpui::{App, ModelHandle, SingletonEntity, WindowId};
 
 use crate::{
     notebooks::{file::is_markdown_file, link::LinkEvent},
     terminal::{model::session::Session, shell::ShellType},
-    util::openable_file_type::FileTarget,
+    test_util::settings::initialize_settings_for_tests,
+    util::{file::external_editor::EditorSettings, openable_file_type::FileTarget},
     workspace::ActiveSession,
 };
+use settings::Setting as _;
 
 use super::{LinkTarget, NotebookLinks, ResolveError, SessionSource};
 
@@ -60,6 +62,8 @@ lazy_static! {
 /// Initialize the app and link resolver. For test purposes, we only care about the base
 /// directory's value, not how it was obtained.
 fn init_link_model(app: &mut App, base_directory: Option<&Path>) -> ModelHandle<NotebookLinks> {
+    initialize_settings_for_tests(app);
+
     let window_id = WindowId::new();
     let source = match base_directory {
         Some(dir) => SessionSource::Target {
@@ -359,7 +363,7 @@ fn test_resolve_file_with_line() {
 }
 
 #[test]
-fn test_open_markdown_file() {
+fn test_open_markdown_file_uses_viewer_when_preferred() {
     let mut root = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
     if !root.join("README.md").exists() {
         root = root.parent().unwrap().to_path_buf();
@@ -395,6 +399,57 @@ fn test_open_markdown_file() {
                 assert!(Arc::ptr_eq(&TEST_SESSION, session));
             }
             other => panic!("Expected OpenFileNotebook event, got {other:?}"),
+        }
+    });
+}
+
+#[test]
+fn test_open_markdown_file_respects_disabled_viewer_preference() {
+    let mut root = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+    if !root.join("README.md").exists() {
+        root = root.parent().unwrap().to_path_buf();
+    }
+
+    App::test((), |mut app| async move {
+        let links = init_link_model(&mut app, Some(&root));
+
+        EditorSettings::handle(&app).update(&mut app, |settings, ctx| {
+            settings
+                .prefer_markdown_viewer
+                .set_value(false, ctx)
+                .unwrap();
+        });
+
+        let events = Arc::new(Mutex::new(vec![]));
+        {
+            let events = events.clone();
+            app.update(|ctx| {
+                ctx.subscribe_to_model(&links, move |_, event, _| {
+                    events.lock().push(event.clone());
+                })
+            });
+        }
+
+        links
+            .update(&mut app, |links, ctx| {
+                let future = links.resolve_and_open("./README.md", ctx);
+                ctx.await_spawned_future(future.future_id())
+            })
+            .await;
+
+        let events = events.lock();
+        assert_eq!(events.len(), 1);
+        match events.first() {
+            Some(LinkEvent::OpenFileWithTarget {
+                path,
+                target,
+                line_col,
+            }) => {
+                assert_eq!(path, &root.join("README.md"));
+                assert_eq!(target, &FileTarget::SystemDefault);
+                assert_eq!(line_col, &None);
+            }
+            other => panic!("Expected OpenFileWithTarget event, got {other:?}"),
         }
     });
 }

timomak/twarp

(cherry picked from commit 8da83b42af32979fae61abe0077d207e51f6fe88)

app/src/notebooks/link_tests.rs
@@ -8,15 +8,17 @@ use lazy_static::lazy_static;
 use parking_lot::Mutex;
 use tempfile::tempdir;
 use twarp_util::path::LineAndColumnArg;
-use twarpui::{App, ModelHandle, WindowId};
+use twarpui::{App, ModelHandle, SingletonEntity, WindowId};
 use url::Url;
 
 use crate::{
     notebooks::{file::is_markdown_file, link::LinkEvent},
     terminal::{model::session::Session, shell::ShellType},
-    util::openable_file_type::FileTarget,
+    test_util::settings::initialize_settings_for_tests,
+    util::{file::external_editor::EditorSettings, openable_file_type::FileTarget},
     workspace::ActiveSession,
 };
+use settings::Setting as _;
 
 use super::{LinkTarget, NotebookLinks, ResolveError, SessionSource};
 
@@ -60,6 +62,8 @@ lazy_static! {
 /// Initialize the app and link resolver. For test purposes, we only care about the base
 /// directory's value, not how it was obtained.
 fn init_link_model(app: &mut App, base_directory: Option<&Path>) -> ModelHandle<NotebookLinks> {
+    initialize_settings_for_tests(app);
+
     let window_id = WindowId::new();
     let source = match base_directory {
         Some(dir) => SessionSource::Target {
@@ -359,7 +363,7 @@ fn test_resolve_file_with_line() {
 }
 
 #[test]
-fn test_open_markdown_file() {
+fn test_open_markdown_file_uses_viewer_when_preferred() {
     let mut root = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
     if !root.join("README.md").exists() {
         root = root.parent().unwrap().to_path_buf();
@@ -395,6 +399,57 @@ fn test_open_markdown_file() {
                 assert!(Arc::ptr_eq(&TEST_SESSION, session));
             }
             other => panic!("Expected OpenFileNotebook event, got {other:?}"),
+        }
+    });
+}
+
+#[test]
+fn test_open_markdown_file_respects_disabled_viewer_preference() {
+    let mut root = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+    if !root.join("README.md").exists() {
+        root = root.parent().unwrap().to_path_buf();
+    }
+
+    App::test((), |mut app| async move {
+        let links = init_link_model(&mut app, Some(&root));
+
+        EditorSettings::handle(&app).update(&mut app, |settings, ctx| {
+            settings
+                .prefer_markdown_viewer
+                .set_value(false, ctx)
+                .unwrap();
+        });
+
+        let events = Arc::new(Mutex::new(vec![]));
+        {
+            let events = events.clone();
+            app.update(|ctx| {
+                ctx.subscribe_to_model(&links, move |_, event, _| {
+                    events.lock().push(event.clone());
+                })
+            });
+        }
+
+        links
+            .update(&mut app, |links, ctx| {
+                let future = links.resolve_and_open("./README.md", ctx);
+                ctx.await_spawned_future(future.future_id())
+            })
+            .await;
+
+        let events = events.lock();
+        assert_eq!(events.len(), 1);
+        match events.first() {
+            Some(LinkEvent::OpenFileWithTarget {
+                path,
+                target,
+                line_col,
+            }) => {
+                assert_eq!(path, &root.join("README.md"));
+                assert_eq!(target, &FileTarget::SystemDefault);
+                assert_eq!(line_col, &None);
+            }
+            other => panic!("Expected OpenFileWithTarget event, got {other:?}"),
         }
     });
 }