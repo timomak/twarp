Drew1811266/Warp-CN

Open Mermaid diagrams in lightbox (#11548)
## Description
Clicking a rendered Mermaid diagram in a notebook now opens the existing
lightbox treatment for that diagram.

This adds a notebook editor action for Mermaid diagram lightbox opens
and dispatches the workspace lightbox action with the rendered diagram
asset.

https://www.loom.com/share/214c562e29cc4ea699965dd03cb909c8

## Linked Issue
- [ ] The linked issue is labeled `ready-to-spec` or
`ready-to-implement`.
- [x] Where appropriate, screenshots or a short video of the
implementation are included below (especially for user-visible or UI
changes).

## Testing
- [x] `cargo fmt -- --check`
- [x] `cargo check`
- [x] `PATH="/tmp/warp-corepack-shims:$PATH" cargo clippy --workspace
--all-targets --all-features --tests -- -D warnings`
- [x] `./script/run` bundled and launched `WarpLocal.app` successfully
- [x] I have manually tested my changes locally with `./script/run`

Feature-specific manual verification: clicked a rendered Mermaid diagram
in a notebook and verified it opens in the existing lightbox treatment.

### Screenshots / Videos
Loom: https://www.loom.com/share/214c562e29cc4ea699965dd03cb909c8

## Agent Mode
- [x] Warp Agent Mode - This PR was created via Warp's AI Agent Mode

CHANGELOG-IMPROVEMENT: Mermaid diagrams in notebooks can now be opened
in a lightbox by clicking the rendered diagram.

Conversation:
https://staging.warp.dev/conversation/76460332-9e09-4b59-b8d0-0cd9b9997f2a

Co-Authored-By: Oz <oz-agent@warp.dev>

---------

Co-authored-by: Oz <oz-agent@warp.dev>

app/src/notebooks/editor/notebook_command.rs
@@ -13,9 +13,11 @@ use syntect::easy::HighlightLines;
 use syntect::highlighting::{self, Theme, ThemeSet};
 use syntect::parsing::SyntaxSet;
 use syntect::util::LinesWithEndings;
+use ui_components::lightbox::{LightboxImage, LightboxImageSource};
 use warp_completer::signatures::CommandRegistry;
 use warp_editor::content::anchor::Anchor;
 use warp_editor::content::buffer::{Buffer, BufferEvent, EditOrigin};
+use warp_editor::content::mermaid_diagram::mermaid_asset_source;
 use warp_editor::content::selection_model::BufferSelectionModel;
 use warp_editor::content::text::{
     BlockType, BufferBlockStyle, CodeBlockType, CODE_BLOCK_DEFAULT_DISPLAY_LANG,
@@ -66,6 +68,7 @@ use crate::view_components::dropdown::DropdownAction;
 use crate::view_components::Dropdown;
 use crate::workflows::workflow::Workflow;
 use crate::workflows::WorkflowType;
+use crate::workspace::WorkspaceAction;
 use crate::ASSETS;
 
 lazy_static! {
@@ -91,6 +94,7 @@ struct MouseStateHandles {
     copy_button_state: MouseStateHandle,
     mermaid_raw_button_state: MouseStateHandle,
     mermaid_rendered_button_state: MouseStateHandle,
+    mermaid_fullscreen_button_state: MouseStateHandle,
 }
 
 struct CachedHighlightKey {
@@ -722,6 +726,43 @@ impl RunnableCommandModel for NotebookCommand {
                 .with_corner_radius(CornerRadius::with_all(Radius::Pixels(4.)))
                 .finish(),
             );
+
+            if !is_raw {
+                let fullscreen_model = self.handle.clone();
+                footer.add_child(
+                    Align::new(
+                        block_footer_action_button(
+                            appearance,
+                            Icon::Maximize,
+                            self.mouse_state_handles
+                                .mermaid_fullscreen_button_state
+                                .clone(),
+                            "Open full screen",
+                            None,
+                        )
+                        .on_click(move |ctx, app, _| {
+                            if let Some(command_model) = fullscreen_model.upgrade(app) {
+                                if let Some(source) = command_model.as_ref(app).command(app) {
+                                    if !source.trim().is_empty() {
+                                        ctx.dispatch_typed_action(WorkspaceAction::OpenLightbox {
+                                            images: vec![LightboxImage {
+                                                source: LightboxImageSource::Resolved {
+                                                    asset_source: mermaid_asset_source(&source),
+                                                },
+                                                description: None,
+                                            }],
+                                            initial_index: 0,
+                                        });
+                                    }
+                                }
+                            }
+                        })
+                        .finish(),
+                    )
+                    .right()
+                    .finish(),
+                );
+            }
         }
         footer.add_child(Shrinkable::new(1.0, Empty::new().finish()).finish());
         footer.add_child(

timomak/twarp

(cherry picked from commit 49bbe78e147bc7f5f811d2894919b9f394cf66e8)

app/src/notebooks/editor/notebook_command.rs
@@ -11,6 +11,8 @@ use syntect::{
     parsing::SyntaxSet,
     util::LinesWithEndings,
 };
+use twarp_editor::content::mermaid_diagram::mermaid_asset_source;
+use ui_components::lightbox::{LightboxImage, LightboxImageSource};
 use twarp_completer::signatures::CommandRegistry;
 use twarp_editor::{
     content::{
@@ -73,6 +75,8 @@ use super::{
     view::EditorViewAction,
     NotebookWorkflow,
 };
+use crate::features::FeatureFlag;
+use crate::workspace::WorkspaceAction;
 
 lazy_static! {
     static ref SUPPORTED_LANGUAGES: &'static [&'static str] = &[
@@ -95,6 +99,7 @@ lazy_static! {
 struct MouseStateHandles {
     insert_button_state: MouseStateHandle,
     copy_button_state: MouseStateHandle,
+    mermaid_fullscreen_button_state: MouseStateHandle,
 }
 
 struct CachedHighlightKey {
@@ -617,6 +622,46 @@ impl RunnableCommandModel for NotebookCommand {
                 .with_vertical_padding(11.)
                 .finish(),
             )
+        }
+        // twarp: upstream's raw/rendered mermaid toggle depends on the (unpicked)
+        // display-mode feature; only the full-screen lightbox button is ported.
+        if matches!(block_style, CodeBlockType::Mermaid)
+            && FeatureFlag::MarkdownMermaid.is_enabled()
+        {
+            let fullscreen_model = self.handle.clone();
+            footer.add_child(
+                Align::new(
+                    block_footer_action_button(
+                        appearance,
+                        Icon::Maximize,
+                        self.mouse_state_handles
+                            .mermaid_fullscreen_button_state
+                            .clone(),
+                        "Open full screen",
+                        None,
+                    )
+                    .on_click(move |ctx, app, _| {
+                        if let Some(command_model) = fullscreen_model.upgrade(app) {
+                            if let Some(source) = command_model.as_ref(app).command(app) {
+                                if !source.trim().is_empty() {
+                                    ctx.dispatch_typed_action(WorkspaceAction::OpenLightbox {
+                                        images: vec![LightboxImage {
+                                            source: LightboxImageSource::Resolved {
+                                                asset_source: mermaid_asset_source(&source),
+                                            },
+                                            description: None,
+                                        }],
+                                        initial_index: 0,
+                                    });
+                                }
+                            }
+                        }
+                    })
+                    .finish(),
+                )
+                .right()
+                .finish(),
+            );
         }
         footer.add_child(Shrinkable::new(1.0, Empty::new().finish()).finish());
         footer.add_child(