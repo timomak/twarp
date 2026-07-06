Drew1811266/Warp-CN

feat(logging): in-session size rotation for warp.log (#10879) (#11000)
Fixes #10879. Independent of #10874 / #10975 — `warp_logging` is a
different engine (env_logger + a pipe target), so this PR can land on
its own.

## What

Adds an orthogonal **in-session** size bound to `warp.log`,
complementing the existing **per-startup** rotation. When the active
`warp.log` exceeds `LogConfig::max_file_size_bytes` mid-session it is
rotated to `warp.log.in_session.0`, existing `.in_session.N` slots shift
up, and the oldest beyond `max_rotation` is discarded. The startup
`.old.N` slots — which log-bundle uploads and crash recovery depend on —
are untouched.

Sketch matches the one in #10879's description: extend `LogConfig`,
plumb to the writer, use `.in_session.N` to distinguish from `.old.N`.

## API

`max_file_size_bytes` defaults to `None`, preserving today's
unbounded-within-session behavior for every existing caller. Adopting
the bound is opt-in:

```rust
warp_logging::init(LogConfig {
    max_file_size_bytes: Some(50 * 1024 * 1024),
    ..Default::default()
})?;
```

Setting a concrete cap on the GUI startup path (e.g. 50 MiB × the
existing `MAX_FILES_IN_GUI_ROTATION` = 5 → ~300 MiB bound per multi-day
session) is intentionally **not part of this PR** — it's a follow-up.
This change only ships the mechanism so the policy decision can be made
separately, with feature-flag staging if desired.

## How

- New `crates/warp_logging/src/rotation.rs`: a `Write` adapter
(`RotatingFileWriter`) that owns a `File`, tracks `bytes_written`, and
rotates pre-write when the next chunk would cross `max_bytes`. Rotation
order: delete oldest slot → shift remaining slots → rename active into
slot 0 → open a fresh active. Matches the overflow semantics of the
existing startup `rotate_files`.
- `crates/warp_logging/src/lib.rs`: `LogConfig.max_file_size_bytes:
Option<u64>`; `LogConfig` now derives `Default`.
- `crates/warp_logging/src/native.rs`: `init_internal` threads the cap
through; the fil

app/src/lib.rs
@@ -651,6 +651,7 @@ pub fn run() -> Result<()> {
                 warp_logging::init(warp_logging::LogConfig {
                     is_cli: true,
                     log_destination: launch_mode.log_destination(),
+                    ..Default::default()
                 })?;
                 return crate::remote_server::run_proxy(args.identity_key.clone());
             }
@@ -791,10 +792,18 @@ fn run_internal(mut launch_mode: LaunchMode) -> Result<()> {
             if crash_recovery::is_crash_recovery_process(launch_mode.args().as_ref()) {
                 warp_logging::init_for_crash_recovery_process()?;
             } else {
-                warp_logging::init(warp_logging::LogConfig { is_cli, log_destination })?;
+                warp_logging::init(warp_logging::LogConfig {
+                    is_cli,
+                    log_destination,
+                    ..Default::default()
+                })?;
             }
         } else {
-            warp_logging::init(warp_logging::LogConfig { is_cli, log_destination })?;
+            warp_logging::init(warp_logging::LogConfig {
+                is_cli,
+                log_destination,
+                ..Default::default()
+            })?;
         }
     }
 

timomak/twarp

(cherry picked from commit c7272dc44b160363fe4ee77301b60232bb185d8b)

app/src/lib.rs
@@ -715,10 +715,18 @@ fn run_internal(mut launch_mode: LaunchMode) -> Result<()> {
             if crash_recovery::is_crash_recovery_process(launch_mode.args().as_ref()) {
                 twarp_logging::init_for_crash_recovery_process()?;
             } else {
-                twarp_logging::init(twarp_logging::LogConfig { is_cli, log_destination })?;
+                twarp_logging::init(twarp_logging::LogConfig {
+                    is_cli,
+                    log_destination,
+                    ..Default::default()
+                })?;
             }
         } else {
-            twarp_logging::init(twarp_logging::LogConfig { is_cli, log_destination })?;
+            twarp_logging::init(twarp_logging::LogConfig {
+                is_cli,
+                log_destination,
+                ..Default::default()
+            })?;
         }
     }
 