warpdotdev/warp

Add integrity checks for DCS hooks. (#25395)
## Description

Adds a client-generated session ID integrity check for DCS lifecycle
hooks so shell-emitted hooks that mutate terminal/session state are only
accepted when they carry a session ID Warp generated and registered for
that session.

This closes the spoofing gap where arbitrary PTY output could emit
lifecycle hooks by:
- Generating non-zero session IDs in the client and injecting them into
local shell, subshell, Docker sandbox, and SSH bootstrap scripts.
- Registering expected session IDs before the corresponding bootstrap
hooks are processed.
- Rejecting missing or unregistered session IDs for state-mutating DCS
hooks.
- Closing the legacy SSH wrapper gap by authenticating the local SSH
hook with the local session ID, registering a client-generated remote
session ID from that authenticated hook, and injecting that remote ID
into the remote bootstrap.
- Keeping hook payload optionality explicit where `Default` needs to
distinguish omitted fields from an explicit legacy/default `0` value.

# Linked Issue

N/A

## Testing

- `cargo fmt --all --manifest-path
/Users/david/src/warp-internal/Cargo.toml`
- `cargo check -p warp --lib --manifest-path
/Users/david/src/warp-internal/Cargo.toml`
- `cargo nextest run -p warp --manifest-path
/Users/david/src/warp-internal/Cargo.toml -E 'test(parse_dcs_ssh)'`
- `cargo nextest run -p warp --manifest-path
/Users/david/src/warp-internal/Cargo.toml -E 'test(parse_dcs_ssh) |
test(test_trim_wsl_err_from_output)'`
- `bash -n app/assets/bundled/bootstrap/bash_body.sh`
- `zsh -n app/assets/bundled/bootstrap/zsh_body.sh`
- `fish -n app/assets/bundled/bootstrap/fish.sh`
- Syntax-checked edited init shell/subshell scripts for bash, zsh, and
fish.
- `git --no-pager diff --check`

Co-Authored-By: Oz <oz-agent@warp.dev>

# Conflicts:
#	app/assets/bundled/bootstrap/zsh_body.sh

# Conflicts:
#	app/assets/bundled/bootstrap/fish.sh

app/src/terminal/local_tty/docker_sandbox.rs
@@ -16,6 +16,7 @@ use std::path::{Path, PathBuf};
 use futures::future::BoxFuture;
 use futures::FutureExt as _;
 use serde::{Deserialize, Serialize};
+use warp_core::SessionId;
 use warpui::{AppContext, SingletonEntity as _};
 
 use super::shell::DirectShellStarter;
@@ -99,6 +100,8 @@ pub struct DockerSandboxShellStarter {
     /// Unique per-instance ID used to derive the container name and host mount
     /// paths. Generated at construction time; see [`Self::new`].
     pub sandbox_id: String,
+    /// The client-generated session ID injected into this sandbox's init script.
+    pub session_id: SessionId,
 }
 
 impl DockerSandboxShellStarter {
@@ -107,10 +110,12 @@ impl DockerSandboxShellStarter {
         // Short random ID — 8 hex chars (32 bits) is plenty for realistic
         // concurrent sandbox counts and keeps container names readable.
         let sandbox_id = format!("{:08x}", rand::random::<u32>());
+        let session_id = direct.session_id();
         Self {
             direct,
             base_image,
             sandbox_id,
+            session_id,
         }
     }
 
@@ -128,6 +133,10 @@ impl DockerSandboxShellStarter {
 
     pub fn base_image(&self) -> Option<&str> {
         self.base_image.as_deref()
+    }
+
+    pub fn session_id(&self) -> SessionId {
+        self.session_id
     }
 
     /// Name passed to `sbx run --name`. Unique per instance.

timomak/twarp

(cherry picked from commit 32d21d15c9a3da1a923d1ed66226cf5cba081d16)

app/src/terminal/local_tty/docker_sandbox.rs
@@ -15,6 +15,7 @@ use futures::FutureExt as _;
 use serde::{Deserialize, Serialize};
 use std::ffi::OsStr;
 use std::path::{Path, PathBuf};
+use twarp_core::SessionId;
 use twarpui::{AppContext, SingletonEntity as _};
 
 use super::shell::DirectShellStarter;
@@ -101,6 +102,8 @@ pub struct DockerSandboxShellStarter {
     /// Unique per-instance ID used to derive the container name and host mount
     /// paths. Generated at construction time; see [`Self::new`].
     pub sandbox_id: String,
+    /// The client-generated session ID injected into this sandbox's init script.
+    pub session_id: SessionId,
 }
 
 impl DockerSandboxShellStarter {
@@ -109,10 +112,12 @@ impl DockerSandboxShellStarter {
         // Short random ID — 8 hex chars (32 bits) is plenty for realistic
         // concurrent sandbox counts and keeps container names readable.
         let sandbox_id = format!("{:08x}", rand::random::<u32>());
+        let session_id = direct.session_id();
         Self {
             direct,
             base_image,
             sandbox_id,
+            session_id,
         }
     }
 
@@ -130,6 +135,10 @@ impl DockerSandboxShellStarter {
 
     pub fn base_image(&self) -> Option<&str> {
         self.base_image.as_deref()
+    }
+
+    pub fn session_id(&self) -> SessionId {
+        self.session_id
     }
 
     /// Name passed to `sbx run --name`. Unique per instance.