warpdotdev/warp

[Security] Remove problematic log statements (#26091)
## Description
This PR targets `security-patches` and removes logging paths that could
emit sensitive URI or authentication payload data.

- Removes incoming and dispatched URI logging that included URL content.
- Avoids incorporating rejected auth redirect URL values into error and
log output across auth entry points.
- Replaces the prior URI redaction-helper coverage with focused
regression tests asserting auth redirect errors never expose query
credentials, state, userinfo, or fragments.

## Security impact
Auth redirect URLs can contain credentials and related sensitive values.
These changes prevent malformed or rejected authentication URI contents
from being emitted through application logging or parse-error paths.

## Testing
- Added regression tests in `app/src/auth/auth_view_modal_tests.rs`
covering wrong-host redirects, missing-credential redirects, and URLs
containing userinfo or fragments.
- GitHub checks report all checks successful (`4 successful`, `10
skipped`, `0 failing`, `0 pending`), including
`semgrep-cloud-platform/scan`.

CHANGELOG-NONE

_Conversation:
https://staging.warp.dev/conversation/499313b2-e662-47ac-8ad0-3b92b4aba793_
_Run:
https://oz.staging.warp.dev/runs/019e6beb-2e5d-77eb-bbc2-1d60c5d151e0_
_This PR was generated with [Oz](https://warp.dev/oz)._

app/src/auth/auth_view_modal.rs
@@ -6,6 +6,7 @@ use pathfinder_geometry::vector::vec2f;
 use url::Url;
 use warp_core::errors::ErrorExt;
 use warp_core::features::FeatureFlag;
+use warp_core::{safe_anyhow, safe_error};
 use warpui::actions::StandardAction;
 use warpui::elements::{
     ChildAnchor, ChildView, Container, Fill, HighlightedHyperlink, MouseStateHandle,
@@ -100,7 +101,10 @@ impl AuthRedirectPayload {
     /// must be of format {scheme}://auth/desktop_redirect?refresh_token={token}.
     pub fn from_url(url: Url) -> Result<Self> {
         if url.host_str() != Some(AUTH_URL_HOST) {
-            return Err(anyhow!("Received URL with unexpected host: {} ", url));
+            return Err(safe_anyhow!(
+                safe: ("Auth redirect URL has unexpected host"),
+                full: ("Received URL with unexpected host: {} ", url)
+            ));
         }
         let query_params: HashMap<_, _> = url.query_pairs().into_owned().collect();
         if let Some(token) = query_params.get(AUTH_URL_REFRESH_TOKEN_QUERY_PARAM) {
@@ -117,9 +121,9 @@ impl AuthRedirectPayload {
                 state: query_params.get(AUTH_URL_STATE_QUERY_PARAM).cloned(),
             })
         } else {
-            Err(anyhow!(
-                "Received URL without refresh token query param: {}",
-                url
+            Err(safe_anyhow!(
+                safe: ("Auth redirect URL is missing required credential"),
+                full: ("Received URL without refresh token query param: {}", url)
             ))
         }
     }
@@ -243,7 +247,10 @@ impl AuthView {
                 });
             }
             Err(error) => {
-                log::error!("Failed to parse AuthRedirectPayload from redirect URL: {error:#}");
+                safe_error!(
+                    safe: ("Failed to parse AuthRedirectPayload from redirect URL"),
+                    full: ("Failed to parse AuthRedirectPayload from redirect URL: {error:#}")
+                );
                 self.last_login_failure_reason =
                     Some(LoginFailureReason::InvalidRedirectUrl { was_pasted: true });
                 self.set_auth_token_input_editable(true, ctx);

app/src/auth/login_slide.rs
@@ -6,6 +6,7 @@ use pathfinder_color::ColorU;
 use pathfinder_geometry::vector::vec2f;
 use ui_components::{button, Component as _, Options as _};
 use warp_core::features::FeatureFlag;
+use warp_core::safe_error;
 use warp_core::ui::theme::color::internal_colors;
 use warp_core::ui::Icon;
 use warpui::actions::StandardAction;
@@ -381,7 +382,10 @@ impl LoginSlideView {
                 });
             }
             Err(error) => {
-                log::error!("Failed to parse AuthRedirectPayload from redirect URL: {error:#}");
+                safe_error!(
+                    safe: ("Failed to parse AuthRedirectPayload from redirect URL"),
+                    full: ("Failed to parse AuthRedirectPayload from redirect URL: {error:#}")
+                );
                 self.last_login_failure_reason =
                     Some(LoginFailureReason::InvalidRedirectUrl { was_pasted: true });
             }

app/src/root_view.rs
@@ -18,6 +18,7 @@ use session_sharing_protocol::common::SessionId;
 use settings::Setting as _;
 use url::Url;
 use warp_core::context_flag::ContextFlag;
+use warp_core::safe_error;
 use warp_core::user_preferences::GetUserPreferences as _;
 use warp_graphql::billing::StripeSubscriptionPlan;
 use warpui::clipboard::ClipboardContent;
@@ -2472,7 +2473,10 @@ impl RootView {
                 });
             }
             Err(error) => {
-                log::error!("Unable to parse AuthResult from url: {error}");
+                safe_error!(
+                    safe: ("Unable to parse AuthResult from url"),
+                    full: ("Unable to parse AuthResult from url: {error}")
+                );
                 self.auth_view.update(ctx, |view, ctx| {
                     view.last_login_failure_reason =
                         Some(LoginFailureReason::InvalidRedirectUrl { was_pasted: false });

timomak/twarp

(cherry picked from commit a7f668eaa3a2e83a3d721ac492fb4e7f3571def7)

app/src/auth/auth_view_modal.rs
@@ -24,6 +24,7 @@ use twarpui::FocusContext;
 use twarpui::SingletonEntity;
 use twarpui::TypedActionView;
 use url::Url;
+use twarp_core::{safe_anyhow, safe_error};
 
 use crate::auth::auth_view_body::AuthViewBody;
 use crate::modal::Modal;
@@ -111,7 +112,10 @@ impl AuthRedirectPayload {
     /// must be of format {scheme}://auth/desktop_redirect?refresh_token={token}.
     pub fn from_url(url: Url) -> Result<Self> {
         if url.host_str() != Some(AUTH_URL_HOST) {
-            return Err(anyhow!("Received URL with unexpected host: {} ", url));
+            return Err(safe_anyhow!(
+                safe: ("Auth redirect URL has unexpected host"),
+                full: ("Received URL with unexpected host: {} ", url)
+            ));
         }
         let query_params: HashMap<_, _> = url.query_pairs().into_owned().collect();
         if let Some(token) = query_params.get(AUTH_URL_REFRESH_TOKEN_QUERY_PARAM) {
@@ -128,9 +132,9 @@ impl AuthRedirectPayload {
                 state: query_params.get(AUTH_URL_STATE_QUERY_PARAM).cloned(),
             })
         } else {
-            Err(anyhow!(
-                "Received URL without refresh token query param: {}",
-                url
+            Err(safe_anyhow!(
+                safe: ("Auth redirect URL is missing required credential"),
+                full: ("Received URL without refresh token query param: {}", url)
             ))
         }
     }
@@ -254,7 +258,10 @@ impl AuthView {
                 });
             }
             Err(error) => {
-                log::error!("Failed to parse AuthRedirectPayload from redirect URL: {error:#}");
+                safe_error!(
+                    safe: ("Failed to parse AuthRedirectPayload from redirect URL"),
+                    full: ("Failed to parse AuthRedirectPayload from redirect URL: {error:#}")
+                );
                 self.last_login_failure_reason =
                     Some(LoginFailureReason::InvalidRedirectUrl { was_pasted: true });
                 self.set_auth_token_input_editable(true, ctx);

app/src/auth/login_slide.rs
@@ -1,3 +1,4 @@
+use twarp_core::safe_error;
 use crate::appearance::Appearance;
 use crate::auth::auth_manager::{AuthManager, AuthManagerEvent};
 use crate::auth::auth_view_modal::AuthRedirectPayload;
@@ -373,7 +374,10 @@ impl LoginSlideView {
                 });
             }
             Err(error) => {
-                log::error!("Failed to parse AuthRedirectPayload from redirect URL: {error:#}");
+                safe_error!(
+                    safe: ("Failed to parse AuthRedirectPayload from redirect URL"),
+                    full: ("Failed to parse AuthRedirectPayload from redirect URL: {error:#}")
+                );
                 self.last_login_failure_reason =
                     Some(LoginFailureReason::InvalidRedirectUrl { was_pasted: true });
             }

app/src/root_view.rs
@@ -1,6 +1,7 @@
 // twarp: 2c-d — AI ServerConversationToken deleted; use stub.
 use crate::app_state::SerializedBlockListItem;
 use crate::app_state::ServerConversationToken;
+use twarp_core::safe_error;
 use crate::appearance::Appearance;
 use crate::auth::auth_manager::{AuthManager, AuthManagerEvent};
 use crate::auth::auth_override_warning_modal::AuthOverrideWarningModalVariant;
@@ -2361,7 +2362,10 @@ impl RootView {
                 });
             }
             Err(error) => {
-                log::error!("Unable to parse AuthResult from url: {error}");
+                safe_error!(
+                    safe: ("Unable to parse AuthResult from url"),
+                    full: ("Unable to parse AuthResult from url: {error}")
+                );
                 self.auth_view.update(ctx, |view, ctx| {
                     view.last_login_failure_reason =
                         Some(LoginFailureReason::InvalidRedirectUrl { was_pasted: false });